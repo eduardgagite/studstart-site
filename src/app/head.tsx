7261ea48bdac0a82249063bd3a2b2faf49edf14f
Eduard <karlodarkblace@gmail.com>
2026-01-31 12:02:17 +0300

    refactor(images): конвертировать PNG в WEBP

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,13 +1,3 @@
-import { assetPath } from "@/lib/assets";
-
 export default function Head() {
-  return (
-    <>
-      <link
-        rel="preload"
-        as="image"
-        href={assetPath("/images/hero-mountains.png")}
-      />
-    </>
-  );
+  return null;
 }
